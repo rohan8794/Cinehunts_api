6b77b980521036af234c88735ff29ece74656ace
rohan8794 <157529525+rohan8794@users.noreply.github.com>
2025-02-12 17:50:01 +0530

    Create server.py

---
+++ server.py
@@ -0,0 +1,29 @@
+
+
+from flask import Flask, jsonify, Response  # Import Response
+from flask_cors import CORS
+from pymongo import MongoClient
+from bson import json_util  # Import json_util from bson
+import json
+
+app = Flask(__name__)
+CORS(app)
+
+# MongoDB connection
+client = MongoClient("mongodb+srv://orgdriphy:ttkCwzgb3yqCdAXN@cluster0.soas0.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+db = client['Testing']
+collection = db['Movies']
+
+@app.route('/api/movies', methods=['GET'])
+def get_movies():
+    # Retrieve movies sorted by newest first, including title and image URL
+    movies_cursor = collection.find({}, {'_id': 1, 'title': 1, 'image': 1}).sort("_id", -1)  # Sort by latest added
+    movies_list = list(movies_cursor)  # Convert cursor to list
+
+    # Use json_util to serialize the data
+    return Response(json.dumps(movies_list, default=json_util.default), mimetype='application/json')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
